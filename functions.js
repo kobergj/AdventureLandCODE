ff68cccf3feb89bbc2071736ce0a9a4b9d6f1408
Julian Koberg <Kokweazel@Julians-MBP.fritz.box>
2016-11-12 01:51:31 +0100

    Added Comments

--- functions.js
+++ functions.js
@@ -1,50 +1,50 @@
 
-/* Safety Parameters */
-var minhp_potion = 500
-var minmp_potion = 500
+/* 
+ 
+    Main Variables
+    
+    More Inside the Code
+ 
+ */
 
-/* Buy Parameters */
+// The Minimal XP a Monster must give to be target
+var MIN_XP_OF_MONSTER = 800
 
+// The Maximal Level your Items are beeing upgraded
+var MAX_ITEM_LEVEL = 6
 
-/* Move Parameters */
-var movementspeed_x = 1
-var movementspeed_y = 2
+// The Json Information to show -- Development
+var SHOW_INFORMATION = character
 
-/* Attack Parameters */
-var attack_target_off = null
+// The Number of Milliseconds for the Main Loop
+var LOOP_SPEED = 250
 
-var monster_maxatk = 120
-var monster_minxp = 100
-var monster_name = null
-var monster_notarget = true
+/* 
 
-/* Show Information */
-// GameInfo: parent.G
-// ItemInfo: character.items
-var show = character
+    Begin of Functions 
 
-/* Starter Function */
-setInterval( Main, 250 ); // All 250 ms
-
-/* Show it Once */
-Show(show)
-
-/* END OF CODE */
+*/
 
 /* Main Functions */
 
+// This Function is called on each Loop
 function Main() {
+    // Don't do anything while Moving
     if (character.moving) { return; }
 
+    // Get target from Server
     var target = get_targeted_monster();
 
+    // Act
     OneLoop(target);
 
     return;
 }
 
+// Action And Order the Character does each round
 function OneLoop(target) {
-    Potion()
+
+    DrinkPotion()
 
     Loot()
 
@@ -64,21 +64,24 @@ function OneLoop(target) {
 
 /* Support Functions */
 
-function Potion() {
+// Min HP to drink Potion
+var minhp_potion = 500
+// Min MP to drink Potion
+var minmp_potion = 500
+
+// Drink a HP or MP Potion
+function DrinkPotion() {
     if (hpLow()) {
         parent.use('hp')
-
-        if (character.items[0].q <= hp_potion_min) {
-            buyHpPotions()
-        }
     }
+
     if (mpLow()) {
-        use_hp_or_mp();
+        parent.use('mp');
     }
 
     return;
 }
-
+// Defines When to drink Hp
 function hpLow() {
     if(character.hp < minhp_potion) {
         return true;
@@ -86,11 +89,7 @@ function hpLow() {
 
     return false;
 }
-
-function buyHpPotions() {
-    buy(hp_potion_name, hp_potion_amount)
-}
-
+// Defines when to drink Mp
 function mpLow() {
     if (character.mp < minmp_potion) {
         return true;
@@ -99,6 +98,9 @@ function mpLow() {
     return false;
 }
 
+/* Loot Functions */
+
+// Loot the chests
 function Loot() {
     loot();
 }
@@ -106,46 +108,112 @@ function Loot() {
 
 /* Attack Functions */
 
+// Attack the Target
 function Attack(target) {
     if (!target) return;
 
     if (!in_attack_range(target)) return;
 
-    // if(safeties && mssince(last_attack)<400) return;
+    if ( target.hp && !target.dead ) {
+        attack(target)
+    }
 
-    set_message("Attacking " + target.mtype)
-    //game_log(parent.next_attack)
+    return;
+}
 
-    if(target.type=="character") parent.player_attack.call(target);
+/* Movement Functions */
 
-    else parent.socket.emit("click",{type:"monster",id:target.server_id,button:"right"}); // parent.monster_attack.call(target);
+// Move in X Direction
+var movementspeed_x = 1
+// Move in Y Direction
+var movementspeed_y = 2
 
-    //last_attack=new Date();
+// Move towards Target If possible
+function Move(target) {
 
-    //attack(target);
+    if (!target) {
+
+        if (attack_target_off!=null){
+            // Go to Friend
+            MoveTo(attack_target_off)
+            return;
+        }
+        // Just Move Around
+        MoveAround()
+        return;
+    }
+
+    if(!in_attack_range(target)) {
+        // Get Closer to it
+        MoveTowards(target)
+        return;
+    }
+
+    return;
+}
+
+// Move to Specific Player
+function MoveTo(player) {
+    set_message("Go to " + player.name)
+
+    move(
+        character.real_x+(player.real_x-character.real_x-1),
+        character.real_y+(player.real_y-character.real_y-1)
+    );
+
+    return;
+}
+
+// Move to a Monster
+function MoveTowards(monster) {
+    game_log("Move half the Distance to monster")
+
+    // Half the Distance? Should be configurable...
+    move(
+        character.real_x+(target.real_x-character.real_x)/2,
+        character.real_y+(target.real_y-character.real_y)/2
+    );
+
+    return;
+}
+
+// Move without target
+function MoveAround() {
+    set_message("No Monsters, bored");
+    // Move according to speed
+    move(
+        character.real_x + movementspeed_x,
+        character.real_y + movementspeed_y
+    );
 
     return;
 }
 
 /* Aiming Functions */
 
+// Name of the player to get target from
+var attack_target_off = null
+
+// Max Atk for Monster
+var monster_maxatk = 200
+// Min XP for Monster
+var monster_minxp = MIN_XP_OF_MONSTER
+// Name of the Monster
+var monster_name = null
+// Get Monster which is 'free'
+var monster_notarget = true
+
+
+// Get New Target if needed
 function CheckTarget(target) {
     if (!target) {
 
-        if (attack_target_off!=null){
-            // Get someones Target
-            player = get_player(attack_target_off);
-            target = get_target_of(player);
-            return;
-        } else {
-            // Get New Monster Targer
-            target=get_nearest_monster({
-                min_xp:monster_minxp,
-                max_att:monster_maxatk,
-                target:monster_name,
-                no_target:monster_notarget
-            });
-        };
+        target = GetTargetOff(attack_target_off)
+
+        if (!target) {
+            // Get New Monster Target
+            target = GetNearestMonster();
+        }
 
         if (target) {
             game_log("Set Target to " + target.mtype)
@@ -157,49 +225,42 @@ function CheckTarget(target) {
     return;
 }
 
-/* Movement Functions */
+// Get someelses Target
+function GetTargetOff(playername) {
+    // Check
+    if (!playername) return null;
+    // Check out the Player
+    player = get_player(playername);
+    // Check out his target
+    target = get_target_of(player);
 
-function Move(target) {
-    if (!target) {
-
-        // Go to Friend
-        if (attack_target_off!=null){
-            set_message("Go to " + player.name)
-            move(
-                character.real_x+(player.real_x-character.real_x-1),
-                character.real_y+(player.real_y-character.real_y-1)
-            );
-            return;
-        }
-
-        // Move according to speed
-        set_message("No Monsters, bored");
-            move(
-                character.real_x + movementspeed_x,
-                character.real_y + movementspeed_y
-            );
-        return;
-    }
-
-    if(!in_attack_range(target)) {
-        game_log("Move half the Distance to monster")
-        move(
-            character.real_x+(target.real_x-character.real_x)/2,
-            character.real_y+(target.real_y-character.real_y)/2
-            );
-        return;
-    }
-
-    return;
+    return target;
 }
 
-/* Update Functions */
-var MaxItemLevel = 6
+// Get Nearest Monster
+function GetNearestMonster() {
 
+    target=get_nearest_monster({
+                min_xp:monster_minxp,
+                max_att:monster_maxatk,
+                target:monster_name,
+                no_target:monster_notarget
+            });
+
+    return target;
+}
+
+
+/* Update Functions */
+
+// The Max Level to wich Items should be updated
+var MaxItemLevel = MAX_ITEM_LEVEL
+// Minimal Money you want to keep
 var MoneySwitch = 100000
 
 function Update() {
 
+    // Don't Update if no Money
     if (character.gold <= MoneySwitch) {
         set_message("Updates Paused")
         return
@@ -207,26 +268,31 @@ function Update() {
 
     // Loop through Equipped Items
     for (slot in character.slots) {
+        // Get the Item
         item = character.slots[slot]
 
+        // Get Name & Level
         var name = item["name"]
         var level = item["level"]
 
-            // Excluding it for the moment
+        // Excluding it for the moment -- Needs a different Scroll
         if (name == "hpamulet") {
             continue;
         }
 
+        // Buy the Item if needed
         BuyItem(name)
-
+        // Update the Item if possible
         UpdateItem(name)
-
+        // Equip the Item if needed
         EquipItem(name, level)
     }
 
 }
 
+// Updates the Item
 function UpdateItem(ItemName) {
+    // Get Item
     item = FindItem(ItemName)
 
     if (!item) {
@@ -234,109 +300,132 @@ function UpdateItem(ItemName) {
         return
     }
 
+    // Max Level?
     if(item["level"] >= MaxItemLevel) {
         return
     }
 
+    // Get the Scroll
     scroll = FindItem(ScrollName)
 
+    // No Scrolls - No Updates
     if (!scroll) {
         return
     }
 
+    // Finally -- Do it!
     upgrade(ItemIndex(ItemName), ItemIndex(ScrollName))
+
+    return;
 }
 
+// Get the Item according to ItemName
 function FindItem(ItemName) {
     // Finds First Item of the Name
-    var i
     for (i=0; i<character.items.length; i++) {
+        // Check out the Item
         item = character.items[i]
-
+        // Does it fit?
         if (item && item["name"] == ItemName) {
-            // game_log("Returning " + i + " And " + item["name"])
-            return item
+            return item;
         }
 
     }
 
-    return null
+    return null;
 }
 
+// Get the Index of the Item -- Desperately looking for a better way
 function ItemIndex(ItemName) {
     // Finds First Index of the Name
-    var i
     for (i=0; i<character.items.length; i++) {
+        // Check out the Item
         item = character.items[i]
-
+        // Does it fit?
         if (item && item["name"] == ItemName) {
-            // game_log("Returning " + i + " And " + item["name"])
-            return i
+            return i;
         }
 
     }
 
-    return null
+    return null;
 }
 
+// Buy the specified Item
 function BuyItem(ItemName) {
+    // Check out Item
     item = FindItem(ItemName)
 
+    // Only buy if you don't have it yet
     if (!item) { 
         game_log("Buying + '" + ItemName + "'")
+        // Buy only One
         buy(ItemName, 1)
-        return
-    }
-
-    return
-}
-
-function EquipItem(ItemName, SlotLevel) {
-    item = FindItem(ItemName)
-
-    if (!item) {
-        return
-    }
-
-    // game_log(SlotLevel)
-    if (item.level > SlotLevel) {
-        equip(ItemIndex(ItemName))
-        game_log("Equiped " + item["name"])
         return;
     }
 
-    return
+    return;
+}
+
+// Equip the Item if its better than SlotLevel
+function EquipItem(ItemName, SlotLevel) {
+    // Check out the item
+    item = FindItem(ItemName)
+
+    // Does it exists?
+    if (!item) return;
+
+    // Is it better?
+    if (item.level > SlotLevel) {
+        game_log("Equipping " + item["name"])
+        equip(ItemIndex(ItemName))
+        return;
+    }
+
+    return;
 }
 
 /* Refill Functions */
-var hp_potion_name = "hpot0"
-var hp_potion_amount = 100
-var hp_potion_min = 100
 
+// Name of the HP Potion
+var HpName = "hpot0"
+// Amount of Potions to buy
+var HpAmount = 100
+// When to buy Potion
+var HpSafety = 100
+
+// The Name of the Scroll to buy
 var ScrollName = "scroll0"
-var ScrollMin = 10
+// The Amount of Scrolls to buy
+var ScrollAmount = 10
+// When to buy Scroll
+var ScrollSafety = 10
 
+// Refill your Stock
 function Refill() {
+    // Buy HP Potions
     RefillHealthPotions()
-
+    // Buy Scrolls
     RefillScrolls()
 }
 
+// Buy HP Potions according to Parameters
 function RefillHealthPotions () {
-    potion = FindItem(hp_potion_name)
+    potion = FindItem(HpName)
 
-    if (!potion || potion["q"] <= hp_potion_min) {
-        buy(hp_potion_name, hp_potion_amount)
+    if (!potion || potion["q"] <= HpSafety) {
+        buy(HpName, HpAmount)
     }
 
-    return
+    return;
 }
 
+// Buy Scrolls
 function RefillScrolls() {
     scroll = FindItem(ScrollName)
 
-    if (!scroll || scroll["q"] <= ScrollMin) {
-        buy(ScrollName, ScrollMin)
+    if (!scroll || scroll["q"] <= ScrollSafety) {
+        buy(ScrollName, ScrollAmount)
     }
 
     return
@@ -344,10 +433,24 @@ function RefillScrolls() {
 
 /* development Functions */
 
+// Show a Json of the Spefied Paramter
 function Show(item) {
     if (item) {
         show_json(item);
     }
 
     return;
-} 
+}
+
+/*
+
+    End of Functions
+
+*/
+
+/* Starter Function */
+setInterval( Main, LOOP_SPEED );
+
+/* Show it Once */
+Show(SHOW_INFORMATION)
+
